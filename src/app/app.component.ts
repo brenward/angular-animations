64cfad4f11af6880dd312f911511efa133a5eb5d
brenward <bren.ward@gmail.com>
2017-07-25 21:15:01 +0100

    finished angular animations section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from "@angular/animations";
+import { trigger, state, style, transition, animate, keyframes, group } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
@@ -40,6 +40,63 @@ import { trigger, state, style, transition, animate } from "@angular/animations"
 			})),
 			animate(500)
 		])
+	]),
+	trigger('list1',[
+		state('in',style({
+			opacity: 1,
+			transform:'translateX(0)'
+		})),
+		transition('void => *', [
+			style({
+				opacity:0,
+				transform:'translateX(-100px)'
+			}),
+			animate(300)]),
+		transition('* => void', [
+			animate(300,style({
+				opacity:0,
+				transform:'translateX(100px)'
+			}))
+		])
+	]),
+	trigger('list2',[
+		state('in',style({
+			opacity: 1,
+			transform:'translateX(0)'
+		})),
+		transition('void => *', [
+			animate(1000,keyframes([
+				style({
+					transform:'translateX(-100px)',
+					opacity: 0,
+					offset:0
+				}),
+				style({
+					transform:'translateX(-50px)',
+					opacity: 0.5,
+					offset:0.3
+				}),
+				style({
+					transform:'translateX(-20px)',
+					opacity: 1,
+					offset:0.8
+				}),
+				style({
+					transform:'translateX(0)',
+					opacity: 1,
+					offset:1
+				})
+			])),
+			animate(300)]),
+		transition('* => void', [
+			group([animate(300,style({
+				color:'red'
+			})),
+			animate(800,style({
+				opacity:0,
+				transform:'translateX(100px)'
+			}))])
+		])
 	])]
 })
 export class AppComponent {
@@ -63,5 +120,13 @@ export class AppComponent {
 
 	onShrink(){
 		this.wildState = 'shrunken';
+	}
+
+	animationStarted(event){
+		console.log(event);
+	}
+
+	animationEnded(event){
+		console.log(event);
 	}
 }
